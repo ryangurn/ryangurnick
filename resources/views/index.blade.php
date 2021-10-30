bb47cd21072057992f578f7de962eea636c387fc
Ryan Gurnick <ryangurnick@gmail.com>
2021-10-30 11:39:41 -0400

    minor tweaks to homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="cards-container space-y-6 my-6">
-        <div class="card-container bg-white rounded-xl flex">
+        <div class="card-container shadow-sm hover:shadow-md transition duration-150 ease-in bg-white rounded-xl flex">
             <div class="border-r border-gray-100 px-5 py-8">
                 <div class="text-left">
                     <span class="text-lg">about</span>
@@ -12,7 +12,7 @@
                 <div class="mx-4">
                     <h4 class="text-xl font-semibold">ryan gurnick</h4>
                     <div class="text-gray-600 mt-3">
-                        I am a technologist who sees the dreams of the world as an opportunity for innovation through machines. Over many years I have worked in various areas to improve my understanding of how technology functions and impacts the world in a meaningful way. I have received a bachelor’s degree in computer information science and computer information technologies while also following my passion for cybersecurity, theatrical productions, and much more. I hope you enjoy my website and learn something new in the process that will spark joy and intrigue in your life.`
+                        I am a technologist who sees the dreams of the world as an opportunity for innovation through machines. Over many years I have worked in various areas to improve my understanding of how technology functions and impacts the world in a meaningful way. I have received a bachelor’s degree in computer information science and computer information technologies while also following my passion for cybersecurity, theatrical productions, and much more. I hope you enjoy my website and learn something new in the process that will spark joy and intrigue in your life.
                     </div>
 
                     <div class="flex items-center justify-between mt-6">
@@ -23,7 +23,7 @@
                         </div>
 
                         <div class="flex items-center space-x-2">
-                            <div class="bg-gray-200 text-xxs font-bold leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                            <div class="bg-gray-100 hover:bg-gray-200 text-xxs font-bold leading-none rounded-full text-center w-28 h-7 py-2 px-4">
                                 edit
                             </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
