33589ecd731d7203bd3d91f790714e4ada87f53d
Ryan Gurnick <ryangurnick@gmail.com>
2021-10-31 22:07:33 -0400

    adding photos

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -47,7 +47,73 @@
             <li>
               <div class="space-y-4">
                 <div class="aspect-w-3 aspect-h-2">
-                  <img class="object-cover shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+                  <img class="object-cover shadow-lg rounded-lg" src="{{ asset('img/1.jpg') }}" alt="">
+                </div>
+
+                <div class="space-y-2">
+                  <div class="text-lg leading-6 font-medium space-y-1">
+                    <p>description here</p>
+                  </div>
+                  <ul role="list" class="flex space-x-5">
+                    <li>
+                      <div class="flex items-center text-gray-400 text-xs font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>home</div>
+                        </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="space-y-4">
+                <div class="aspect-w-3 aspect-h-2">
+                  <img class="object-cover shadow-lg rounded-lg" src="{{ asset('img/2.jpg') }}" alt="">
+                </div>
+
+                <div class="space-y-2">
+                  <div class="text-lg leading-6 font-medium space-y-1">
+                    <p>description here</p>
+                  </div>
+                  <ul role="list" class="flex space-x-5">
+                    <li>
+                      <div class="flex items-center text-gray-400 text-xs font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>home</div>
+                        </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="space-y-4">
+                <div class="aspect-w-3 aspect-h-2">
+                  <img class="object-cover shadow-lg rounded-lg" src="{{ asset('img/3.jpg') }}" alt="">
+                </div>
+
+                <div class="space-y-2">
+                  <div class="text-lg leading-6 font-medium space-y-1">
+                    <p>description here</p>
+                  </div>
+                  <ul role="list" class="flex space-x-5">
+                    <li>
+                      <div class="flex items-center text-gray-400 text-xs font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>home</div>
+                        </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="space-y-4">
+                <div class="aspect-w-3 aspect-h-2">
+                  <img class="object-cover shadow-lg rounded-lg" src="{{ asset('img/4.jpg') }}" alt="">
                 </div>
 
                 <div class="space-y-2">
