435c3ec7af7fdea89aef5e00109ae7960e1953d9
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-01 22:14:23 -0400

    adding onclick and mouse events

--- app/Http/Livewire/Core/CardFooter.php
+++ app/Http/Livewire/Core/CardFooter.php
@@ -6,6 +6,18 @@ use Livewire\Component;
 
 class CardFooter extends Component
 {
+    public $show = false;
+
+    public function hidePopup()
+    {
+        $this->show = false;
+    }
+
+    public function showPopup()
+    {
+        $this->show = true;
+    }
+
     public function render()
     {
         return view('livewire.core.card-footer');
--- resources/views/livewire/core/card-footer.blade.php
+++ resources/views/livewire/core/card-footer.blade.php
@@ -1,15 +1,15 @@
 <div class="flex items-center justify-between mt-6">
     <livewire:core.footer-metadata />
 
-    <div class="flex items-center space-x-2">
+    <div class="flex items-center space-x-2" x-init="$wire.show">
         <div class="bg-gray-100 hover:bg-gray-200 text-xxs font-bold leading-none rounded-full text-center w-28 h-7 py-2 px-4">
             edit
         </div>
-        <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+        <button wire:click="$toggle('show')" x-data="{ hide(){ $wire.hidePopup() }, show() { $wire.showPopup() } }" x-on:click.away="hide()" x-on:mouseover="show()" x-on:mouseleave="hide()" class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
             <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
-            <ul class="hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
-                 <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
-                 <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+            <ul class="{{ ($show) ? '' : 'hidden' }} absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-1 px-1 ml-8 shadow-sm hover:shadow-md">
+                 <li><a href="#" class="hover:bg-gray-100 rounded-xl block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
+                 <li><a href="#" class="hover:bg-gray-100 rounded-xl block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
             </ul>
         </button>
     </div>
