bac39d520222c49f282b82520a89513f90cf08d8
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-01 22:15:36 -0400

    debouncing mouse events

--- resources/views/livewire/core/card-footer.blade.php
+++ resources/views/livewire/core/card-footer.blade.php
@@ -5,7 +5,7 @@
         <div class="bg-gray-100 hover:bg-gray-200 text-xxs font-bold leading-none rounded-full text-center w-28 h-7 py-2 px-4">
             edit
         </div>
-        <button wire:click="$toggle('show')" x-data="{ hide(){ $wire.hidePopup() }, show() { $wire.showPopup() } }" x-on:click.away="hide()" x-on:mouseover="show()" x-on:mouseleave="hide()" class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+        <button wire:click="$toggle('show')" x-data="{ hide(){ $wire.hidePopup() }, show() { $wire.showPopup() } }" x-on:click.away="hide()" x-on:mouseover.debounce="show()" x-on:mouseleave.debounce="hide()" class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
             <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
             <ul class="{{ ($show) ? '' : 'hidden' }} absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-1 px-1 ml-8 shadow-sm hover:shadow-md">
                  <li><a href="#" class="hover:bg-gray-100 rounded-xl block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
