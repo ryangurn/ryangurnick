963f94f489347afd5a4594e22afd96c48fad6e4c
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-07 22:42:44 -0500

    fixing margin if location exists

--- resources/views/livewire/photo/photo-grid.blade.php
+++ resources/views/livewire/photo/photo-grid.blade.php
@@ -10,7 +10,7 @@
         <div class="space-y-2">
           <div class="flex">
             @if (isset($photo['location']) && $photo['location'] != null)
-            <span class="float-right inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 ml-2">
+            <span class="float-right inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
                 <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-green-400" fill="currentColor" viewBox="0 0 8 8">
                     <circle cx="4" cy="4" r="3" />
                 </svg>
@@ -18,7 +18,7 @@
             </span>
             @endif
             @if (isset($photo['date']) && $photo['date'] != null)
-            <span class="float-right inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800 ml-2">
+            <span class="float-right inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800 {{ (isset($photo['location']) && $photo['location'] != null) ? 'ml-2' : '' }}">
                 <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-yellow-400" fill="currentColor" viewBox="0 0 8 8">
                     <circle cx="4" cy="4" r="3" />
                 </svg>
@@ -31,7 +31,7 @@
           </div>
           <ul role="list" class="flex space-x-5">
             <li>
-              <livewire:core.footer-metadata />
+              <livewire:core.footer-metadata :duration="$updated_at" />
             </li>
           </ul>
         </div>
