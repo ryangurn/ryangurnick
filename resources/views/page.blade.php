c62ab6bee65bddcff4cf8c381fdd0b20c596d281
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-13 23:23:44 -0500

    adding page title

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+    <x-slot name="title">
+        {{ $page->title . ' | ' }}ryangurnick
+    </x-slot>
+
     <div class="cards-container space-y-6 my-6">
         @if(!$modules->isEmpty())
             @foreach($modules as $module)
