dc71df6e80dd9686e957bca272b1fe2fff443507
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-13 23:07:56 -0500

    deleting old code

--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <div class="cards-container space-y-6 my-6">
-        <livewire:home.about-card />
-
-        <livewire:home.quote-card />
-
-        <livewire:home.project-card />
-    </div>
-</x-app-layout>
--- resources/views/photos.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    <div class="cards-container space-y-6 my-6">
-        <livewire:photo.gallery-card />
-
-        <livewire:photo.photo-grid />
-    </div>
-</x-app-layout>
