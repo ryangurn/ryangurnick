d0962de2c08c3db92543385203593712c22d2508
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-15 19:29:57 -0500

    adding draft label

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,16 @@
 <div class="card-container shadow-sm hover:shadow-md transition duration-150 ease-in bg-white rounded-xl flex {{ ($page_module->enabled) ? '' : 'border-dashed border-4' }}">
     <div class="border-r border-gray-100 px-5 py-8 w-1/5">
-        <div class="text-left">
-            <span class="text-lg">{{ $title }}</span>
+        <div class="text-left grid grid-cols-1">
+            <div>
+                <span class="text-lg">{{ $title }}</span>
+            </div>
+            @if (!$page_module->enabled)
+                <div>
+                    <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
+                        Draft
+                    </span>
+                </div>
+            @endif
         </div>
     </div>
 
