b4b1e32c92daa393733043acdc44c17fe9e3ed61
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-20 17:45:39 -0500

    adding edit contact card

---
+++ app/Http/Livewire/Core/edit/EditContact.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Livewire\Core\edit;
+
+use App\Models\PageModule;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
+use LivewireUI\Modal\ModalComponent;
+
+class EditContact extends ModalComponent
+{
+    public $page_module;
+
+    public $header;
+
+    public function mount()
+    {
+        $this->module = PageModule::where('id', '=', $this->page_module['id'])->first()->module;
+    }
+
+    public function rules()
+    {
+        return $this->module->parameters;
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $header = $this->module->module_parameters->where('hash', '=', $this->page_module['hash'])->where('parameter', '=', 'header')->first();
+
+        $header->value = $this->header;
+        $header->save();
+
+        $this->module->updated_at = Carbon::now();
+        $this->module->save();
+
+        $this->closeModal();
+        $this->redirect(URL::previous());
+    }
+
+    public function render()
+    {
+        return view('livewire.core.edit.edit-contact');
+    }
+}
