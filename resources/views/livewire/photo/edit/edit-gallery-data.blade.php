c68019eb286ff38968d2406c5077cc59d25a41cd
Ryan Gurnick <ryangurnick@gmail.com>
2021-11-28 21:32:35 -0800

    adding shortcuts to photos livewire components
    cmd+s and ctrl+s

--- resources/views/livewire/photo/edit/edit-gallery-data.blade.php
+++ resources/views/livewire/photo/edit/edit-gallery-data.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data>
     <div class="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
         <label for="name" class="sr-only">name</label>
         <input type="text" name="name" id="name" class="block w-full border-0 pt-2.5 text-lg font-medium placeholder-gray-500 focus:ring-0" placeholder="Title" wire:model="name">
@@ -18,7 +18,11 @@
     <div class="absolute bottom-0 inset-x-px">
         <div class="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
             <div class="flex-shrink-0">
-                <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="save">
+                <button @keydown.window.prevent.ctrl.s="$wire.save()"
+                        @keydown.window.prevent.cmd.s="$wire.save()"
+                        type="submit"
+                        class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        wire:click="save">
                     Save
                 </button>
             </div>
