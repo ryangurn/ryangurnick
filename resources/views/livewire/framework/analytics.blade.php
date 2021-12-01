02383d0d59c2832d1891901e4fe30fb71d9f2ae8
ryan gurnick <ryangurnick@gmail.com>
2021-12-01 18:31:27 -0500

    finishing basic analytics

--- resources/views/livewire/framework/analytics.blade.php
+++ resources/views/livewire/framework/analytics.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <dl class="grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
+    <dl class="grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-x divide-gray-200 md:grid-cols-3 md:divide-y md:divide-x">
         @if (!$pages->isEmpty())
             @foreach ($pages as $page)
         <div class="px-4 py-5 sm:p-6">
@@ -13,20 +13,14 @@
                         unique views today
                     </span>
                 </div>
-
-                <div class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
-                    <!-- Heroicon name: solid/arrow-sm-up -->
-                    <svg class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                    </svg>
-                    <span class="sr-only">
-                        Increased by
-                    </span>
-                    {{ $page->statistic_views()->where('created_at', '>', \Carbon\Carbon::now()->addDays(-1)->startOfDay())->where('created_at', '<', \Carbon\Carbon::now()->addDays(-1)->endOfDay())->get()->count() }}
-                </div>
             </dd>
         </div>
             @endforeach
+                @if ($pages->count() % 3 != 0)
+                    @for ($i=$pages->count(); ($i % 3 != 0); $i++ )
+                        <div class="px-4 py-5 sm:p-6"></div>
+                    @endfor
+                @endif
         @endif
     </dl>
 </div>
