77718f1d19685b6eecfbf1725b5a8c1faa995af0
ryan gurnick <ryangurnick@gmail.com>
2021-12-04 13:36:47 -0500

    working on email slideover

---
+++ app/Http/Livewire/Framework/EmailSlideover.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire\Framework;
+
+use App\Models\Email;
+use Livewire\Component;
+
+class EmailSlideover extends Component
+{
+    protected $listeners = ['show' => 'show', 'hide' => 'hide'];
+
+    public $show = false;
+
+    public $emails;
+
+    public function mount()
+    {
+        $this->emails = Email::all();
+    }
+
+    public function show()
+    {
+        $this->show = true;
+    }
+
+    public function hide()
+    {
+        $this->show = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.framework.email-slideover');
+    }
+}
