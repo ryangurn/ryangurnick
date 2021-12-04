7fe8a71c0ae256e49cb9b1843bb5364804b6603c
ryan gurnick <ryangurnick@gmail.com>
2021-12-03 22:04:56 -0500

    adding react button

--- resources/views/livewire/photo/view-photo.blade.php
+++ resources/views/livewire/photo/view-photo.blade.php
@@ -20,7 +20,14 @@
             @endif
         </div>
         <div class="text-lg leading-6 font-medium space-y-1">
-            <p>{{ $photo->caption }}</p>
+            <p>
+                {{ $photo->caption }}
+            </p>
+            <p>
+                <button type="button" class="mt-4 mb-4 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    react
+                </button>
+            </p>
         </div>
     </div>
 </div>
