e35a4b9ecd8a1feebab00966ef4ad5ac4969aabb
ryan gurnick <ryangurnick@gmail.com>
2021-12-08 19:06:01 -0500

    checking for explode two or larger
    this allows for initials to be used.

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,6 @@
 @if (\App\Models\Setting::where('key', '=', 'application.logo')->first() != null && \App\Models\Image::where('id', '=', \App\Models\Setting::where('key', '=', 'application.logo')->first()->value)->first() != null)
 <img src="{{ \App\Models\Image::where('id', '=', \App\Models\Setting::where('key', '=', 'application.logo')->first()->value)->first()->file }}" alt="avatar" {{ $attributes->class('mr-2 rounded-full') }} />
-@elseif (count(explode(" ", \App\Models\Setting::where('key', '=', 'sitename')->first()->value)) == 2)
+@elseif (count(explode(" ", \App\Models\Setting::where('key', '=', 'sitename')->first()->value)) >= 2)
     <div class="text-3xl font-bold rounded-full border border-gray-400 p-2 mr-2">
         {{ substr(explode(" ", \App\Models\Setting::where('key', '=', 'sitename')->first()->value)[0], 0, 1).' '.substr(explode(" ", \App\Models\Setting::where('key', '=', 'sitename')->first()->value)[1], 0, 1) }}
     </div>
