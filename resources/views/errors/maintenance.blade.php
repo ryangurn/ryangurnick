cd69e17d033185c37524fca2ad5e5c1543b66cac
ryan gurnick <ryangurnick@gmail.com>
2021-12-12 12:57:49 -0500

    adjusting maintenance page to use proper http response code

--- resources/views/errors/maintenance.blade.php
+++ resources/views/errors/maintenance.blade.php
@@ -16,7 +16,7 @@
     <div class="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div class="max-w-max mx-auto">
             <main class="sm:flex">
-                <p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
+                <p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">503</p>
                 <div class="sm:ml-6">
                     <div class="sm:border-l sm:border-gray-200 sm:pl-6">
                         <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Maintenance taking place</h1>
