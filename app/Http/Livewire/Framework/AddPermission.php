6ecab66103845c60018590c498ad96695347f776
ryan gurnick <ryangurnick@gmail.com>
2021-12-19 00:15:09 -0500

    creating the add permission modal

---
+++ app/Http/Livewire/Framework/AddPermission.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Livewire\Framework;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\URL;
+use LivewireUI\Modal\ModalComponent;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+/**
+ * AddPermission is a livewire modal component that
+ * provides the ability to associate a permission to
+ * a specific role.
+ */
+class AddPermission extends ModalComponent
+{
+    /**
+     * Provide authorization functionality for permissions
+     * verification.
+     */
+    use AuthorizesRequests;
+
+    /**
+     * the value that allows for the role to be pulled
+     * from the database based on the identifier
+     * @var
+     */
+    public $role_id;
+
+    /**
+     * the value that stores the role modal that is being
+     * updated.
+     * @var
+     */
+    public $role;
+
+    /**
+     * the array that stores all the permissions models.
+     * @var
+     */
+    public $permissions;
+
+    /**
+     * the value that stores the permission to associate
+     * to the role.
+     * @var
+     */
+    public $permission;
+
+    /**
+     * function that is called when the livewire component is
+     * initialized.
+     * @return void
+     */
+    public function mount()
+    {
+        $this->role = Role::where('id', $this->role_id)->first();
+        $this->permissions = Permission::whereNotIn('id', $this->role->permissions->pluck('id'))->get();
+    }
+
+    /**
+     * validation rules that will be checked when the
+     * update role modal is saved.
+     * @return string[]
+     */
+    public function rules()
+    {
+        return ['permission' => 'required|numeric|exists:permissions,id'];
+    }
+
+    /**
+     * the function that when called will associate a
+     * permission to a role.
+     * @return void
+     * @throws AuthorizationException
+     */
+    public function save()
+    {
+        // verify authorization
+        $this->authorize('associate permissions');
+
+        // validate the request
+        $this->validate();
+
+        // associate the permission to the role
+        $this->role->givePermissionTo($this->permission);
+
+        // close the modal and redirect
+        $this->closeModal();
+        $this->redirect(URL::previous());
+    }
+
+    /**
+     * the method that is automatically called to render
+     * the view for the livewire component.
+     * @return Application|Factory|View
+     */
+    public function render()
+    {
+        return view('livewire.framework.add-permission');
+    }
+}
---
+++ resources/views/livewire/framework/add-permission.blade.php
@@ -0,0 +1,42 @@
+<div x-data>
+    <div class="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+        <label for="name" class="sr-only">permission</label>
+        <select class="block w-full border-0 pt-2.5 text-lg font-medium placeholder-gray-500 focus:ring-0" wire:model="permission">
+            @if(!$permissions->isEmpty())
+                @foreach($permissions as $permission)
+                    <option value="{{ $permission->id }}">{{ $permission->name }}</option>
+                @endforeach
+            @else
+                <option value="-1">no permissions to associate</option>
+            @endif
+        </select>
+
+        <div aria-hidden="true">
+            <div class="h-px"></div>
+            <div class="py-2">
+                <div class="py-px">
+                    <div class="h-9"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="absolute bottom-0 inset-x-px">
+        <div class="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
+            <div class="flex-shrink-0">
+                <button @keydown.window.prevent.ctrl.s="$wire.save()"
+                        @keydown.window.prevent.cmd.s="$wire.save()"
+                        type="submit"
+                        class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        wire:click="save">
+                    Save
+                </button>
+            </div>
+            <div>
+                <ul class="list-disc">
+                    @error('permission') <li class="text-red-800">{{ $message }}</li> @enderror
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
