11857d941234d5b04f329ad5dd85ce6eb89dcf79
ryan gurnick <ryangurnick@gmail.com>
2021-12-18 21:32:59 -0500

    adding no contacts message

--- resources/views/livewire/framework/email-slideover.blade.php
+++ resources/views/livewire/framework/email-slideover.blade.php
@@ -61,7 +61,18 @@
                                                 </div>
                                             </div>
                                         @endforeach
-                                    @endif
+                                    @else
+                                        <!-- This example requires Tailwind CSS v2.0+ -->
+                                            <button type="button" class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+                                                </svg>
+                                                <span class="mt-2 block text-sm font-medium text-gray-900">
+                                                    no contacts yet
+                                                </span>
+                                            </button>
+
+                                        @endif
                                     </div>
                                 </div>
                             </div>
