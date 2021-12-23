125debf3a19d40dded6a95e05ae715b9df43e0ee
ryan gurnick <ryangurnick@gmail.com>
2021-12-23 13:05:42 -0500

    adding module loop component

---
+++ app/View/Components/ModuleLoop.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Page;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class ModuleLoop extends Component
+{
+    public $modules;
+
+    public $identifier;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(Page $page, $identifier = "")
+    {
+        // get modules on page
+        $this->modules = $page->page_modules->sortBy('order');
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View|Closure|string
+     */
+    public function render()
+    {
+        return view('components.module-loop');
+    }
+}
