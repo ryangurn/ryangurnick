2f81c49115094872ca75ac02b541004017104b41
ryan gurnick <ryangurnick@gmail.com>
2021-12-24 02:52:50 -0500

    fixing Undefined array key 1

--- resources/views/livewire/framework/email-slideover.blade.php
+++ resources/views/livewire/framework/email-slideover.blade.php
@@ -1,81 +1,84 @@
-@can('view emails')
-<div x-data="{show: @entangle('show')}">
-    <div x-show="show" class="fixed inset-0 overflow-hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-        <div class="absolute inset-0 overflow-hidden">
-            <div class="absolute inset-0" aria-hidden="true">
-                <div x-show="show" class="fixed inset-y-0 right-0 pl-10 max-w-full flex"
-                     x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700"
-                     x-transition:enter-start="translate-x-full"
-                     x-transition:enter-end="translate-x-0"
-                     x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700"
-                     x-transition:leave-start="translate-x-0"
-                     x-transition:leave-end="translate-x-full">
+<div>
+    @can('view emails')
+        <div x-data="{show: @entangle('show')}">
+            <div x-show="show" class="fixed inset-0 overflow-hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+                <div class="absolute inset-0 overflow-hidden">
+                    <div class="absolute inset-0" aria-hidden="true">
+                        <div x-show="show" class="fixed inset-y-0 right-0 pl-10 max-w-full flex"
+                             x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700"
+                             x-transition:enter-start="translate-x-full"
+                             x-transition:enter-end="translate-x-0"
+                             x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700"
+                             x-transition:leave-start="translate-x-0"
+                             x-transition:leave-end="translate-x-full">
 
-                    <div class="w-screen max-w-md">
-                        <div @click.away="show = false" class="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                            <div class="px-4 sm:px-6">
-                                <div class="mt-4 flex items-start justify-between">
-                                    <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
-                                        contacts
-                                    </h2>
-                                    <div class="ml-3 h-7 flex items-center">
-                                        <button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="hide">
-                                            <span class="sr-only">Close panel</span>
-                                            <!-- Heroicon name: outline/x -->
-                                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                            </svg>
-                                        </button>
+                            <div class="w-screen max-w-md">
+                                <div @click.away="show = false" class="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                                    <div class="px-4 sm:px-6">
+                                        <div class="mt-4 flex items-start justify-between">
+                                            <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
+                                                contacts
+                                            </h2>
+                                            <div class="ml-3 h-7 flex items-center">
+                                                <button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="hide">
+                                                    <span class="sr-only">Close panel</span>
+                                                    <!-- Heroicon name: outline/x -->
+                                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="mt-6 relative flex-1 px-4 sm:px-6">
-                                <div class="absolute inset-0 px-4 sm:px-6">
-                                    <div class="accordion accordion-flush">
-                                    @if (!$emails->isEmpty())
-                                        @foreach($emails as $email)
-                                            <div class="accordion-item border-t-0 border-l-0 border-r-0 rounded-none bg-white border border-gray-200" x-data="{ show_{{ $email->id }}: false }">
-                                                <h2 class="accordion-header mb-0">
-                                                    <button class="accordion-button relative flex items-center w-full py-4 px-5 text-base text-gray-800 text-left bg-white border-0 rounded-none transition focus:outline-none gap-2" type="button" x-on:click="show_{{ $email->id }} = ! show_{{ $email->id }}">
+                                    <div class="mt-6 relative flex-1 px-4 sm:px-6">
+                                        <div class="absolute inset-0 px-4 sm:px-6">
+                                            <div class="accordion accordion-flush">
+                                                @if (!$emails->isEmpty())
+                                                    @foreach($emails as $email)
+                                                        <div class="accordion-item border-t-0 border-l-0 border-r-0 rounded-none bg-white border border-gray-200" x-data="{ show_{{ $email->id }}: false }">
+                                                            <h2 class="accordion-header mb-0">
+                                                                <button class="accordion-button relative flex items-center w-full py-4 px-5 text-base text-gray-800 text-left bg-white border-0 rounded-none transition focus:outline-none gap-2" type="button" x-on:click="show_{{ $email->id }} = ! show_{{ $email->id }}">
                                                         <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 text-gray-800">
                                                             {{ $email->parameters['first_name'] }} {{ $email->parameters['last_name'] }}
                                                         </span>
-                                                        @if ($email->parameters['company'] != null)
-                                                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-green-100 text-green-800">
+                                                                    @if ($email->parameters['company'] != null)
+                                                                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-green-100 text-green-800">
                                                             {{ $email->parameters['company'] }}
                                                         </span>
-                                                        @endif
-                                                        @if ($email->parameters['phone_number'] != null)
-                                                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-blue-100 text-blue-800">
+                                                                    @endif
+                                                                    @if ($email->parameters['phone_number'] != null)
+                                                                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-blue-100 text-blue-800">
                                                             {{ $email->parameters['phone_number'] }}
                                                         </span>
-                                                        @endif
-                                                    </button>
-                                                </h2>
-                                                <div x-show="show_{{ $email->id }}" class="accordion-collapse border-0 collapse show">
-                                                    <div class="accordion-body py-4 px-5">
-                                                        <iframe class="w-full h-100" src="{{ route('mailable', $email) }}"></iframe>
-                                                        @can('read emails')
-                                                        <a href="#" wire:click="read({{ $email->id }})" class="mt-4 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-yellow-100 text-yellow-800">
-                                                            mark as read
-                                                        </a>
-                                                        @endcan
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        @endforeach
-                                    @else
-                                        <!-- This example requires Tailwind CSS v2.0+ -->
-                                            <button type="button" class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" viewBox="0 0 24 24" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-                                                </svg>
-                                                <span class="mt-2 block text-sm font-medium text-gray-900">
+                                                                    @endif
+                                                                </button>
+                                                            </h2>
+                                                            <div x-show="show_{{ $email->id }}" class="accordion-collapse border-0 collapse show">
+                                                                <div class="accordion-body py-4 px-5">
+                                                                    <iframe class="w-full h-100" src="{{ route('mailable', $email) }}"></iframe>
+                                                                    @can('read emails')
+                                                                        <a href="#" wire:click="read({{ $email->id }})" class="mt-4 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-yellow-100 text-yellow-800">
+                                                                            mark as read
+                                                                        </a>
+                                                                    @endcan
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    @endforeach
+                                                @else
+                                                <!-- This example requires Tailwind CSS v2.0+ -->
+                                                    <button type="button" class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                        <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+                                                        </svg>
+                                                        <span class="mt-2 block text-sm font-medium text-gray-900">
                                                     no contacts yet
                                                 </span>
-                                            </button>
+                                                    </button>
 
-                                        @endif
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -84,6 +87,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    @endcan
+
 </div>
-@endcan
--- resources/views/livewire/framework/user-slideover.blade.php
+++ resources/views/livewire/framework/user-slideover.blade.php
@@ -1,58 +1,60 @@
-@can('view access controls')
-<div x-data="{show: @entangle('show')}">
-    <div x-show="show" class="fixed inset-0 overflow-hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-        <div class="absolute inset-0 overflow-hidden">
-            <div class="absolute inset-0" aria-hidden="true">
-                <div x-show="show" class="fixed inset-y-0 right-0 pl-10 max-w-full flex"
-                     x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700"
-                     x-transition:enter-start="translate-x-full"
-                     x-transition:enter-end="translate-x-0"
-                     x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700"
-                     x-transition:leave-start="translate-x-0"
-                     x-transition:leave-end="translate-x-full">
+<div>
+    @can('view access controls')
+    <div x-data="{show: @entangle('show')}">
+        <div x-show="show" class="fixed inset-0 overflow-hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+            <div class="absolute inset-0 overflow-hidden">
+                <div class="absolute inset-0" aria-hidden="true">
+                    <div x-show="show" class="fixed inset-y-0 right-0 pl-10 max-w-full flex"
+                         x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700"
+                         x-transition:enter-start="translate-x-full"
+                         x-transition:enter-end="translate-x-0"
+                         x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700"
+                         x-transition:leave-start="translate-x-0"
+                         x-transition:leave-end="translate-x-full">
 
-                    <div class="w-screen max-w-md">
-                        <div @click.away="show = false" class="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                            <div class="px-4 sm:px-6">
-                                <div class="mt-4 flex items-start justify-between">
-                                    <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
-                                        users
-                                    </h2>
-                                    <div class="ml-3 h-7 flex items-center">
-                                        <button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="hide">
-                                            <span class="sr-only">Close panel</span>
-                                            <!-- Heroicon name: outline/x -->
-                                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                            </svg>
-                                        </button>
+                        <div class="w-screen max-w-md">
+                            <div @click.away="show = false" class="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                                <div class="px-4 sm:px-6">
+                                    <div class="mt-4 flex items-start justify-between">
+                                        <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
+                                            users
+                                        </h2>
+                                        <div class="ml-3 h-7 flex items-center">
+                                            <button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="hide">
+                                                <span class="sr-only">Close panel</span>
+                                                <!-- Heroicon name: outline/x -->
+                                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                                </svg>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="mt-6 relative flex-1 px-4 sm:px-6">
-                                <div class="absolute inset-0 px-4 sm:px-6">
-                                    @if (!$users->isEmpty())
-                                        <div class="grid grid-cols-1 gap-4">
-                                            @foreach($users as $user)
-                                                <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                                    <div class="flex-shrink-0">
-                                                        <img class="h-10 w-10 rounded-full" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?d=mp" alt="">
+                                <div class="mt-6 relative flex-1 px-4 sm:px-6">
+                                    <div class="absolute inset-0 px-4 sm:px-6">
+                                        @if (!$users->isEmpty())
+                                            <div class="grid grid-cols-1 gap-4">
+                                                @foreach($users as $user)
+                                                    <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                                        <div class="flex-shrink-0">
+                                                            <img class="h-10 w-10 rounded-full" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?d=mp" alt="">
+                                                        </div>
+                                                        <div class="flex-1 min-w-0">
+                                                            <a href="#" wire:click="$emit('openModal', 'framework.user-modal', {{ json_encode(['user_id' => $user->id]) }})" class="focus:outline-none">
+                                                                <span class="absolute inset-0" aria-hidden="true"></span>
+                                                                <p class="text-sm font-medium text-gray-900">
+                                                                    {{ $user->name }}
+                                                                </p>
+                                                                <p class="text-sm text-gray-500 truncate">
+                                                                    {{ implode(", ", $user->roles->pluck('name')->toArray()) }}
+                                                                </p>
+                                                            </a>
+                                                        </div>
                                                     </div>
-                                                    <div class="flex-1 min-w-0">
-                                                        <a href="#" wire:click="$emit('openModal', 'framework.user-modal', {{ json_encode(['user_id' => $user->id]) }})" class="focus:outline-none">
-                                                            <span class="absolute inset-0" aria-hidden="true"></span>
-                                                            <p class="text-sm font-medium text-gray-900">
-                                                                {{ $user->name }}
-                                                            </p>
-                                                            <p class="text-sm text-gray-500 truncate">
-                                                                {{ implode(", ", $user->roles->pluck('name')->toArray()) }}
-                                                            </p>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                    @endif
+                                                @endforeach
+                                            </div>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -61,5 +63,5 @@
             </div>
         </div>
     </div>
+    @endcan
 </div>
-@endcan
