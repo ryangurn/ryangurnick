790b60775153d787eea715ebc99147f527ee4d79
ryan gurnick <ryangurnick@gmail.com>
2021-12-24 02:38:18 -0500

    undoing stupid bluehost changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // ensure key lengths are not too long
-        Schema::defaultStringLength(191);
+        //
     }
 }
