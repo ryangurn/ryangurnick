9cd1fe54a672237e3ef57562ce075b4c77bc2fd1
ryan gurnick <ryangurnick@gmail.com>
2021-12-25 14:19:00 -0500

    updating banner to use modals

--- app/Http/Livewire/Framework/Banner.php
+++ app/Http/Livewire/Framework/Banner.php
@@ -52,61 +52,11 @@ class Banner extends Component
     public $auth = false;
 
     /**
-     * the value that stores all the module models.
-     * @var
-     */
-    public $modules;
-
-    /**
-     * the value populated when adding a page.
-     * @var
-     */
-    public $page_name;
-
-    /**
-     * the value that stores the current page model.
-     * @var
-     */
-    public $page;
-
-    /**
-     * the value that stores all the pages models.
-     * @var
-     */
-    public $pages;
-
-    /**
-     * the value that stores the current page id.
-     * @var
-     */
-    public $page_id;
-
-    /**
-     * the value populated when adding a page to the
-     * menu.
-     * @var
-     */
-    public $menu_id;
-
-    /**
      * the variable that stores all pages that can be
      * added to the menu.
      * @var
      */
     public $menu_options;
-
-    /**
-     * the value that is populated when selecting a module
-     * to add to a page.
-     * @var
-     */
-    public $module_id;
-
-    /**
-     * the value that stores all the gallery models.
-     * @var
-     */
-    public $galleries;
 
     /**
      * the array that stores all the allowed
@@ -116,207 +66,29 @@ class Banner extends Component
     public $allowed_modules = [];
 
     /**
+     * the value that stores the current page model.
+     * @var
+     */
+    public $page;
+
+    /**
      * function that is called when the livewire component is
      * initialized.
      * @return void
      */
     public function mount()
     {
+        // populate all the variables
+        $this->auth = Auth::check();
+        $in_menu = PageNavigation::all()->pluck('page_id');
+        $this->menu_options = Page::whereNotIn('id', $in_menu)->where('name', '<>', 'post')->get();
+
         // grab the modules for the page type
         $type_modules = PageTypeModule::where('type_id', $this->page->type_id)->get();
         foreach($type_modules as $module)
         {
             $this->allowed_modules[] = $module->module->component;
         }
-
-        // populate all the variables
-        $this->auth = Auth::check();
-        $this->modules = Module::whereIn('component', $this->allowed_modules)->where('component', '!=', 'photo.photo-grid')->get()->sortBy('name');
-        $this->pages = Page::all()->sortBy('name');
-        $this->galleries = Gallery::all()->sortBy('name');
-
-        $in_menu = PageNavigation::all()->pluck('page_id');
-        $this->menu_options = Page::whereNotIn('id', $in_menu)->where('name', '<>', 'post')->get();
-
-        $this->module_id = $this->pages->first()->id;
-        if (!$this->menu_options->isEmpty())
-        {
-            $this->menu_id = $this->menu_options->first()->id;
-        }
-
-    }
-
-    /**
-     * a function that will refresh the page.
-     * @return void
-     */
-    public function refresh()
-    {
-        $this->redirect(URL::previous());
-    }
-
-    /**
-     * the function that when called will add
-     * a new page using the $page variable.
-     * @return void
-     * @throws AuthorizationException
-     */
-    public function add_page()
-    {
-        // verify authorization
-        $this->authorize('add page');
-
-        if ($this->page_name != 'blog')
-        {
-            // get the standard page type
-            $standard = PageType::where('name', '=', 'standard')->first();
-
-            // add the page
-            $page = Page::create([
-                'type_id' => $standard->id,
-                'title' => $this->page_name,
-                'slug' => '/'.Str::slug($this->page_name),
-                'name' => Str::slug($this->page_name),
-                'controller' => 'App\Http\Controllers\PageController',
-                'method' => 'index',
-                'publish_date' => Carbon::now()
-            ]);
-        }
-        else
-        {
-            // get the standard page type
-            $blog = PageType::where('name', '=', 'blog')->first();
-
-            // add the page
-            $page = Page::create([
-                'type_id' => $blog->id,
-                'title' => $this->page_name,
-                'slug' => '/'.Str::slug($this->page_name),
-                'name' => Str::slug($this->page_name),
-                'controller' => 'App\Http\Controllers\PageController',
-                'method' => 'index',
-                'publish_date' => Carbon::now()
-            ]);
-        }
-
-        // refresh the current page.
-        $this->refresh();
-    }
-
-    /**
-     * the function that when called will
-     * add a module to the current page.
-     * @return void
-     * @throws AuthorizationException
-     */
-    public function add_module()
-    {
-        // verify authorization
-        $this->authorize('add module');
-
-        // get the module using the id
-        $module = Module::where('id', '=', $this->module_id)->first();
-
-        // determine the new order number
-        $order = PageModule::where('page_id', '=', $this->page->id)->orderBy('order', 'desc')->first();
-        $new_order = (($order != null) ? $order->order : 0) + 10;
-
-        // setup a dynamic module hash
-        $hash = md5(time());
-
-        // add post page if a post is added and there is not already a post page.
-        $post_check = Page::where('name', 'post')->count();
-        if ($module->component == "blog.post" && $post_check == 0)
-        {
-            $post_type = PageType::where('name', '=', 'post')->first();
-            $post = Page::firstOrCreate([
-                'type_id' => $post_type->id,
-                'title' => 'post',
-                'slug' => '/post/{page_module:hash}',
-                'name' => 'post',
-                'controller' => 'App\Http\Controllers\PageController',
-                'method' => 'index',
-                'publish_date' => Carbon::now()
-            ]);
-
-            $post_module = new PageModule();
-            $post_module->module_id = $module->id;
-            $post_module->page_id = $post->id;
-            $post_module->order = $new_order;
-            $post_module->hash = $hash;
-            $post_module->enabled = true;
-            $post_module->save();
-        }
-
-        // add the module to a page.
-        $page_module = new PageModule();
-        $page_module->module_id = $module->id;
-        $page_module->page_id = $this->page->id;
-        $page_module->order = $new_order;
-        // handle dynamic modules
-        if ($module->dynamic) {
-            // generate a hash and store it
-            $page_module->hash = $hash;
-
-            // loop through all the parameters and create rows for them
-            foreach ($module->parameters as $parameter => $rule)
-            {
-                $param = new ModuleParameter();
-                $param->module_id = $module->id;
-                $param->parameter = $parameter;
-                $param->hash = $hash;
-                $param->value = (is_array($module->examples[$parameter])) ? json_encode($module->examples[$parameter]) : $module->examples[$parameter];
-                $param->save();
-            }
-        }
-        $page_module->save();
-
-        // refresh the page.
-        $this->refresh();
-    }
-
-    /**
-     * the function that when called will add a page to the
-     * menu using $menu_id
-     * @return void
-     * @throws AuthorizationException
-     */
-    public function add_menu()
-    {
-        // verify authorization
-        $this->authorize('edit menu');
-
-        // add the page to the menu
-        PageNavigation::create([
-            'page_id' => $this->menu_id,
-            'enabled' => true
-        ]);
-
-        // refresh the page
-        $this->refresh();
-    }
-
-    /**
-     * the function that when called will delete a page.
-     * @return void
-     * @throws AuthorizationException
-     */
-    public function delete_page()
-    {
-        // verify authorization
-        $this->authorize('delete page');
-
-        // first delete any menu references
-        PageNavigation::where('page_id', '=', $this->page_id)->delete();
-
-        // delete all the modules from the page.
-        PageModule::where('page_id', '=', $this->page_id)->delete();
-
-        // delete the page.
-        Page::where('id', '=', $this->page_id)->delete();
-
-        // fresh the page.
-        $this->refresh();
     }
 
     /**
