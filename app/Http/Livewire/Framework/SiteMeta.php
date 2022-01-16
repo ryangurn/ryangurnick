36b4fece4ce244649f246d88c6e810711d09c24c
Ryan Gurnick <ryangurnick@gmail.com>
2022-01-15 22:27:58 -0500

    adding indexing options

---
+++ app/Http/Livewire/Framework/SiteMeta.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Livewire\Framework;
+
+use App\Models\Setting;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+/**
+ * SiteMeta is a livewire component that exists
+ * to provide meta tags that optimize for SEO.
+ */
+class SiteMeta extends Component
+{
+    /**
+     * the variable stores the current indexing
+     * options.
+     * @var
+     */
+    public $indexing;
+
+    /**
+     * function that is called when the livewire component is
+     * initialized.
+     * @return void
+     */
+    public function mount()
+    {
+        $this->indexing = (Setting::where('key', 'application.index')->first() != null) ? Setting::where('key', 'application.index')->first()->value : 'none';
+    }
+
+    /**
+     * the method that is automatically called to render
+     * the view for the livewire component.
+     * @return Application|Factory|View
+     */
+    public function render()
+    {
+        return view('livewire.framework.site-meta');
+    }
+}
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="@yield('additional_html_css')">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <livewire:framework.site-meta />
 
     @yield('title')
 
