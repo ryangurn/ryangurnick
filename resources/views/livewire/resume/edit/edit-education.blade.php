1de87889b29464c2894630b45da463af6468b94e
Ryan Gurnick <ryangurnick@gmail.com>
2022-01-20 17:44:53 -0500

    adding darkmode support for education edit

--- resources/views/livewire/resume/edit/edit-education.blade.php
+++ resources/views/livewire/resume/edit/edit-education.blade.php
@@ -12,27 +12,26 @@
                 @endif
 
                 <div class="col-span-5">
-                    <fieldset class="bg-white">
-                        <legend class="block text-sm font-medium text-gray-700">education #{{ $key+1 }}</legend>
+                    <fieldset class="bg-white dark:bg-gray-900">
+                        <legend class="block text-sm font-medium text-gray-700 dark:text-gray-400">education #{{ $key+1 }}</legend>
                         <div class="mt-1 rounded-md shadow-sm -space-y-px">
                             <div>
-                                <label for="country" class="sr-only">organization #{{ $key+1 }}</label>
-                                <input type="text" class="focus:ring-indigo-500 focus:border-indigo-500 relative block w-full rounded-none rounded-t-md bg-transparent focus:z-10 sm:text-sm border-gray-300" placeholder="organization #{{ $key+1 }}" wire:model="institutions.{{ $key }}.organization">
+                                <label for="country" class="sr-only dark:text-gray-400">organization #{{ $key+1 }}</label>
+                                <input type="text" class="dark:border-gray-700 dark:text-gray-400 dark:bg-gray-900 dark:placeholder-gray-300 focus:ring-indigo-500 focus:border-indigo-500 relative block w-full rounded-none rounded-t-md bg-transparent focus:z-10 sm:text-sm border-gray-300" placeholder="organization #{{ $key+1 }}" wire:model="institutions.{{ $key }}.organization">
                             </div>
                             <div>
-                                <label for="postal-code" class="sr-only">duration #{{ $key+1 }}</label>
-                                <input type="text" class="focus:ring-indigo-500 focus:border-indigo-500 relative block w-full rounded-none bg-transparent focus:z-10 sm:text-sm border-gray-300" placeholder="duration #{{ $key+1 }}" wire:model="institutions.{{ $key }}.duration">
+                                <label for="postal-code" class="sr-only dark:text-gray-400">duration #{{ $key+1 }}</label>
+                                <input type="text" class="dark:border-gray-700 dark:text-gray-400 dark:bg-gray-900 dark:placeholder-gray-300 focus:ring-indigo-500 focus:border-indigo-500 relative block w-full rounded-none bg-transparent focus:z-10 sm:text-sm border-gray-300" placeholder="duration #{{ $key+1 }}" wire:model="institutions.{{ $key }}.duration">
                             </div>
                             <div>
-                                <label for="postal-code" class="sr-only">description #{{ $key+1 }}</label>
-                                <textarea class="focus:ring-indigo-500 focus:border-indigo-500 relative block w-full rounded-none rounded-b-md bg-transparent focus:z-10 sm:text-sm border-gray-300" placeholder="description #{{ $key+1 }}" wire:model="institutions.{{ $key }}.body"></textarea>
+                                <label for="postal-code" class="sr-only dark:text-gray-400">description #{{ $key+1 }}</label>
+                                <textarea class="dark:border-gray-700 dark:text-gray-400 dark:bg-gray-900 dark:placeholder-gray-300 focus:ring-indigo-500 focus:border-indigo-500 relative p-3 block w-full rounded-none rounded-b-md bg-transparent focus:z-10 sm:text-sm border-gray-300" placeholder="description #{{ $key+1 }}" wire:model="institutions.{{ $key }}.body"></textarea>
                             </div>
                         </div>
                     </fieldset>
                 </div>
 
                 <div class="col-span-1">
-                    <label for="region" class="block text-sm font-medium text-gray-700 mt-1">&nbsp;</label>
                     <button type="submit" class="inline-flex items-center px-2 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" wire:click="remove({{ $key }})">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
@@ -62,7 +61,7 @@
 
 
     <div class="absolute bottom-0 inset-x-px">
-        <div class="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
+        <div class="border-t border-gray-200 dark:border-gray-700 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
             <div class="flex-shrink-0">
                 <button @keydown.window.prevent.ctrl.s="$wire.save()"
                         @keydown.window.prevent.cmd.s="$wire.save()"
